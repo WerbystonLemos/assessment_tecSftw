2c3b61b7139a8a179767a0314222625d85c1cf47
WerbystonLemos <werbyston@hotmail.com>
2024-07-27 23:28:48 -0300

    [Fix] Conserto de rotas e fronts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <x-app-layout>
+        Aboute ME
+    </x-app-layout>
+</div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,18 +16,13 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-dropdown-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                                        this.closest('form').submit();" class="flex text-white">
-
-                        <button id="btnSair" class="btn btn-link">
+                        <button id="btnSair" class="btn btn-link" :href="route('logout')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0z"/>
                             <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
                             </svg>
                             Sair
                         </button>
-                    </x-dropdown-link>
                 </form>
             </div>
         </div>
