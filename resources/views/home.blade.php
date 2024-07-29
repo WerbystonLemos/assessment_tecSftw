5541e1c989fe65137d834466e26ec25b838841ac
WerbystonLemos <werbyston@hotmail.com>
2024-07-29 02:17:17 -0300

    [Feature] um tela a respeito de mim

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,79 @@
 <div>
     <x-app-layout>
-        Aboute ME
+
+        <div class="mt-3">
+            <a href="https://github.com/WerbystonLemos" target="_blank" class="btn btn-primary">
+                Github
+            </a>
+            <a href="https://linkedin.com/in/werbyston-lemos" target="_blank" class="btn btn-primary">
+                Linkedin
+            </a>
+        </div>
+
+        <hr>
+
+        <div class="row mt-5">
+            <div class="col-3">
+
+                <h3>Formações</h3>
+                <ul>
+                    <li>Anális de Sistemas</li>
+                    <li>Engenharia de Software <br/>(<i>Latu-sensu</i>)</li>
+                </ul>
+                <h3 class="text-gray-700">Habilidades</h3>
+                <ul>
+                    <li>Laravel</li>
+                    <li>Bootstrap</li>
+                    <li>PHP</li>
+                    <li>Yii</li>
+                    <li>PHP Unit</li>
+                    <li>SQL</li>
+                    <li>JQuery</li>
+                    <li>Javascript</li>
+                    <li>ReactJS</li>
+                    <li>HTML/CSS</li>
+                    <li>ExpressJS</li>
+                    <li>NodeJS</li>
+                    <li>Lumen</li>
+                </ul>
+
+            </div>
+
+            <div class="col-9 ">
+                <h3 class="text-gray-700">Experiências</h3>
+                <ul>
+                    <li>
+                        <h6><strong>Healthbit Performas Sys Tecnologia e Inteligência LTDA.</strong></h6>
+                        <h5>Desenvolvedor – PHP/Yii Framework</h5> 08/2023 – 02/2024<br/>
+                        <p class="text-justify">
+                            Desenvolvi junto com minha equipe um sistema interno e externo para monitoramento e gerenciamento de planos de saúde. Portal esse que consumia dados de Data Lake. Trabalhei no desenvolvimento e manutenção de API’s. Trabalhei com as tecnologias PHP, Yii Framework, MySQL, Bootstrap 5, CSS3, Jquery, JS, Ajax, Apache Airflow.
+                        </p><br/>
+                    </li>
+                    <li>
+                        <h6><strong>CLAMED - Cia. Latina Americana de Medicamentos</strong></h6>
+                        <h5>Desenvolvedor – PHP/ReactJs</h5>
+                        04/2022 – 08/2023<br/>
+                        <p class="text-justify">Desenvolvi e mantive módulos do sistema da intranet bem como manutenção em sites existentes. Uso de stacks como Bootstrap, PHP, Jquery, Ajax, JSON, ReactJs, Axios, linguagens Web (HTML CSS e JS)</p><br/>
+                    </li>
+                    <li>
+                        <h6><strong>WireLink - Fortaleza Telec. LTDA.</strong></h6>
+                        <h5>Desenvolvedor FullStack – Laravel</h5>
+                        02/2021 – 12/2021<br/>
+                        <p class="text-justify">
+                            Desenvolvimento e manutenção de sistemas de uso interno com uso de recursos de integração entre sistemas com utilização de REST e SOAP para o mesmo. Uso de stacks como Bootstrap, Laravel (v5 e v8), Jquery, Ajax, JSON, VueJs, Axios e linguagens Web.
+                        </p><br/>
+                    </li>
+                    <li>
+                        <h6><strong>Inova Energy Engenharia LTDA.</strong></h6>
+                        <h5>Desenvolvedor FullStack</h5>
+                        04/2019 – 01/2021<br/>
+                        <p class="text-justify">
+                            Desenvolvimento, manutenção e suporte de sistema proprietário com forte vivência em Bootstrap (v4), JQuery, PHP, SGBD Firebird, requisições Ajax, JSON, vivência com DELPHI/Pascal e linguagens WEB (HTML5, CSS e JS).
+                        </p><br/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
     </x-app-layout>
 </div>
